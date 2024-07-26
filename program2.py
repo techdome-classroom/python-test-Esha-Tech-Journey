2101443b637fb50e12f23521a2fdce3d6845d01e
Esha-Tech-Journey <eshajawade12@gmail.com>
2024-07-26 12:21:18 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -17,6 +17,6 @@ def decode_message( s: str, p: str) -> bool:
         return match_decoder_key(message[1:], pattern[1:])
     else:
         # Check if the current letters match
-        return message[0] == pattern[0] and match_decoder_key(message[1:], pattern[1:])"
+        return message[0] == pattern[0] and match_decoder_key(message[1:], pattern[1:])""
   
         return False
