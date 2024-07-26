3b2e462262fe39c82c1fd225c7f679359b4d1b0c
Esha-Tech-Journey <eshajawade12@gmail.com>
2024-07-26 12:23:14 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,7 +1,7 @@
 def decode_message( s: str, p: str) -> bool:
 
 # write your code here
- def match_decoder_key(s, pat):
+ def match_decoder_key(s, p):
     if len(s) == 0 and len(p) == 0:
         return True
     if len(s) == 0 or len(p) == 0:
