58282c067e9755ff202cc515164a8ab128632a7c
Esha-Tech-Journey <eshajawade12@gmail.com>
2024-07-26 12:23:37 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,4 +1,4 @@
-def decode_message( s: str, p: str) -> bool:
+def decode_s( s: str, p: str) -> bool:
 
 # write your code here
  def match_decoder_key(s, p):
@@ -9,14 +9,14 @@ def decode_message( s: str, p: str) -> bool:
 
     if p[0] == '*':
         # Try matching the star symbol with no letters, one letter, or multiple letters
-        return match_decoder_key(message, pattern[1:]) or \
-               match_decoder_key(message[1:], pattern) or \
-               match_decoder_key(message[1:], pattern[1:])
+        return match_decoder_key(s, pattern[1:]) or \
+               match_decoder_key(s[1:], pattern) or \
+               match_decoder_key(s[1:], pattern[1:])
     elif pattern[0] == '?':
         # Try matching the question mark with any single letter
-        return match_decoder_key(message[1:], pattern[1:])
+        return match_decoder_key(s[1:], pattern[1:])
     else:
         # Check if the current letters match
-        return message[0] == pattern[0] and match_decoder_key(message[1:], pattern[1:])#
+        return s[0] == pattern[0] and match_decoder_key(s[1:], pattern[1:])#
   
         return False
