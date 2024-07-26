491091bfa7ad204f702dd6f98f7d7cdcc4877da2
Esha-Tech-Journey <eshajawade12@gmail.com>
2024-07-26 12:24:57 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -8,14 +8,14 @@ def decode_message( s: str, p: str) -> bool:
 
     if p[0] == '*':
         # Try matching the star symbol with no letters, one letter, or multiple letters
-        return de(s, p[1:]) or \
-               de(s[1:], p) or \
-               de(s[1:], p[1:])
+        return decode_message(s, p[1:]) or \
+               decode_message(s[1:], p) or \
+               decode_message(s[1:], p[1:])
     elif p[0] == '?':
         # Try matching the question mark with any single letter
-        return de(s[1:], p[1:])
+        return decode_message(s[1:], p[1:])
     else:
         # Check if the current letters match
-        return s[0] == p[0] and de(s[1:], p[1:])#
+        return s[0] == p[0] and decode_message(s[1:], p[1:])#
   
         return False
